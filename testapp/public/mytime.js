8fee577860c564bf081a7a0dffb47bf41eab1477
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2021-08-12 20:58:04 +1000

    I spent way to long doing this

--- testapp/public/mytime.js
+++ testapp/public/mytime.js
@@ -1,65 +1,13 @@
+var DateTime = luxon.DateTime;
+
 setInterval(showTime, 1000);
   function showTime() {
-    let time = new Date();
-    let mytime = time.toLocaleString(undefined, { timezone: "Australia/Brisbane" })
-    let myhour = mytime.getHours();
-    let myhour2 = mytime.getHours();
-    let mymin = mytime.getMinutes();
-    let mysec = mytime.getSeconds();
-    let myday = mytime.getDay();
-    let mydates = mytime.getDate();
-    let mymonth = mytime.getMonth();
-    let myyear = mytime.getFullYear();
+    let time = DateTime.now().setZone("Australia/Brisbane");
+    let format = time.toFormat('hh:mm:ss a, ccc DD')
+    let formatted = ("Server Time<br />" + format)
 
-    am_pm = " AM";
-    if (myhour > 12) {
-      myhour -= 12;
-      am_pm = " PM";
-    }
-    if (myhour == 0) {
-      hr = 12;
-      am_pm = " AM";
+    document.getElementById("mytime")
+      .innerHTML = formatted
     }
 
-    myhour = myhour < 10 ? "0" + myhour : myhour;
-    mymin = mymin < 10 ? "0" + mymin : mymin;
-    mysec = mysec < 10 ? "0" + mysec : mysec;
-
-    let currentTime = myhour + ":"
-        + mymin + ":" + mysec + am_pm;
-
-    let currentTime24hr = myhour2 + ":"
-        + mymin + ":" + mysec
-
-    let days = {
-      0: "Sun",
-      1: "Mon",
-      2: "Tue",
-      3: "Wed",
-      4: "Thu",
-      5: "Fri",
-      6: "Sat"
-    }
-
-    let months = {
-      0: "Jan",
-      1: "Feb",
-      2: "Mar",
-      3: "Apr",
-      4: "May",
-      5: "Jun",
-      6: "Jul",
-      7: "Aug",
-      8: "Sep",
-      9: "Oct",
-      10: "Nov",
-      11: "Dec"
-    }
-
-    let currentDate = `${days[myday]} ${mydates} ${months[mymonth]} ${myyear}`;
-
-            document.getElementById("mytime")
-                .innerHTML = `Server Time<br />${currentTime} ${currentDate}`
-        }
-
-        showTime();
+showTime();
