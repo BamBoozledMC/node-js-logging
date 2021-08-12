908249279cc5e85dde9896116ef0cc84292f3e9f
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2021-08-12 21:30:46 +1000

    Comment link

--- testapp/public/time.js
+++ testapp/public/time.js
@@ -3,6 +3,7 @@ var DateTime = luxon.DateTime;
 setInterval(showTime, 1000);
   function showTime() {
     let time = DateTime.now();
+    // https://moment.github.io/luxon/index.html#/formatting?id=table-of-tokens
     let format = time.toFormat('hh:mm:ss a, ccc DD')
     let formatted = ("Your Time<br />" + format)
 
